5e754c3b9f444c5fc84d581bc1ff3dfd22a64bbf
Mateus Neres <mateusneresrb@gmail.com>
2020-04-17 03:36:37 -0300

    Update frontend design

--- level-01/frontend/src/App.js
+++ level-01/frontend/src/App.js
@@ -1,27 +1,47 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import { FiTrash2 } from 'react-icons/fi';
+import {
+  IoLogoGithub,
+  IoLogoJavascript,
+  IoLogoNodejs,
+  IoLogoHtml5,
+} from 'react-icons/io';
 import api from './services/api';
 
-import "./styles.css";
+import './App.css';
+import Logo from './assets/logo.png';
 
-function App() {
+export default function App() {
   const [repositories, setRepositories] = useState([]);
-  
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+  const [techs, setTechs] = useState('');
+
   useEffect(() => {
-    api.get('repositories').then(response => {
-     setRepositories(response.data);
+    api.get('repositories').then((response) => {
+      setRepositories(response.data);
     });
   }, []);
 
-  async function handleAddRepository() {
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (title === '' || url === '' || techs === '') {
+      console.log('Any information is empty!');
+      return;
+    }
+    setTitle('');
+    setUrl('');
+    setTechs('');
 
-     const response = await api.post('repositories', {
-      title: 'Novo projeto1',
-      owner: 'Testando'
-     });
+    const repository = {
+      title,
+      url,
+      techs: techs.replace(/ /g, '').split(','),
+    };
 
-    const repository = response.data;
+    const response = await api.post('repositories', repository);
 
-    setRepositories([...repositories, repository]);
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
@@ -31,20 +51,62 @@ function App() {
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-          {repositories.map(repository => (
-          <li key={repository.id}>{repository.title}
-          <button onClick={() => handleRemoveRepository(repository.id)}>
-            Remover
-          </button>
-          </li>
-          ))}
-      </ul>
+    <div className="page-container">
+      <header className="header-container">
+        <img className="logo" alt="Repository logo" src={Logo} />
+      </header>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          placeholder="Title"
+        />
+        <input
+          onChange={(e) => setUrl(e.target.value)}
+          value={url}
+          placeholder="Url"
+        />
+        <input
+          onChange={(e) => setTechs(e.target.value)}
+          value={techs}
+          placeholder="Techs"
+        />
+
+        <button type="submit">Cadastrar</button>
+      </form>
+
+      <ul>
+        {repositories.length > 0 ? (
+          repositories.map((repository) => (
+            <li key={repository.id}>
+              <div>
+                <IoLogoGithub size={140} color="#000" />
+              </div>
+
+              <strong>{repository.title}</strong>
+
+              <p>{repository.url}</p>
+
+              {repository.techs.length < 4 ? (
+                <section>
+                  <IoLogoHtml5 className="icons" size={32} />
+                  <IoLogoJavascript className="icons" size={32} />
+                  <IoLogoNodejs className="icons" size={32} />
+                </section>
+              ) : (<div />)}
+
+              <button onClick={() => handleRemoveRepository(repository.id)} type="button">
+                <FiTrash2 size={23} />
+              </button>
+            </li>
+          ))) : (
+            <div id="empty_repository">
+              {console.log('Repositories is empty')}
+            </div>
+        )}
+
+      </ul>
     </div>
   );
 }
-
-export default App;
